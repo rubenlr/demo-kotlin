38cd0b20c899b0787c024a4b61efb403f618217f
Rúben Lício Reis <rubenlr@gmail.com>
2024-08-04 20:59:09 +0300

    Transactions Repo

--- exchange/src/test/kotlin/com/rubenlr/demo/FakeDataProvider.kt
+++ exchange/src/test/kotlin/com/rubenlr/demo/FakeDataProvider.kt
@@ -1,16 +1,17 @@
 package com.rubenlr.demo
 
-import com.rubenlr.demo.data.entities.Account
-import com.rubenlr.demo.data.entities.Asset
-import com.rubenlr.demo.data.entities.AssetType
-import com.rubenlr.demo.data.entities.User
+import com.rubenlr.demo.data.entities.*
 import io.bloco.faker.Faker
 import java.math.BigDecimal
 import java.math.RoundingMode
+import java.time.OffsetDateTime
 import kotlin.random.Random
 
 object FakeDataProvider {
     private val faker = Faker()
+
+    private fun nextDecimal(min: Double = 1.0, max: Double = 10000.0) =
+        BigDecimal.valueOf(Random.nextDouble(min, max)).setScale(2, RoundingMode.HALF_UP)
 
     private fun getUser(userId: Long = 1) = User(userId, faker.name.name(), faker.internet.email())
 
@@ -24,8 +25,8 @@ object FakeDataProvider {
 
     fun getAssets(size: Long = 2) = (1..size).map { assetId -> getAsset(assetId) }
 
-    private fun getAccount(user: User, asset: Asset, accountId: Long = 1L, balance: Double = Random.nextDouble(1.0, 1000.0)) =
-        Account(accountId, user, asset, BigDecimal.valueOf(balance).setScale(2, RoundingMode.HALF_UP))
+    private fun getAccount(user: User, asset: Asset, accountId: Long = 1L) =
+        Account(accountId, user, asset, nextDecimal())
 
     fun getAccounts(users: List<User>, assets: List<Asset>): List<Account> {
         val userAssetList = users.flatMap { user -> assets.map { asset -> user to asset } }.toSet().shuffled()
@@ -34,5 +35,48 @@ object FakeDataProvider {
             val (user, asset) = userAssetList[accountId - 1]
             getAccount(user, asset, accountId.toLong())
         }
+    }
+
+    private fun getTransaction(from: Account, to: Account, transactionId: Long) =
+        Transaction(
+            transactionId,
+            from,
+            nextDecimal(),
+            TransactionType.EXCHANGE,
+            to,
+            nextDecimal(),
+            OffsetDateTime.now()
+        )
+
+    fun getTransactions(accounts: List<Account>): List<Transaction> {
+        var transactionId = 1L
+        val transactions = mutableListOf<Transaction>()
+
+        accounts.shuffled().forEach { from ->
+            // Don't want `from` and `to` to be equal
+            accounts.filter { it != from }.shuffled().forEach { to ->
+                repeat(Random.nextInt(2, 5)) {
+                    transactions.add(getTransaction(from, to, transactionId++))
+                }
+            }
+        }
+
+        return transactions
+    }
+
+    data class TransactionDto(
+        val users: List<User>,
+        val assets: List<Asset>,
+        val accounts: List<Account>,
+        val transactions: List<Transaction>
+    )
+
+    fun getTransactions(usersSize: Long, assetsSize: Long): TransactionDto {
+        val users = getUsers(usersSize)
+        val assets = getAssets(assetsSize)
+        val accounts = getAccounts(users, assets)
+        val transactions = getTransactions(accounts)
+
+        return TransactionDto(users, assets, accounts, transactions)
     }
 }
--- exchange/src/test/kotlin/com/rubenlr/demo/repositories/TransactionRepositoryTest.kt
+++ exchange/src/test/kotlin/com/rubenlr/demo/repositories/TransactionRepositoryTest.kt
@@ -1,14 +1,48 @@
 package com.rubenlr.demo.repositories
 
+import com.rubenlr.demo.FakeDataProvider
+import com.rubenlr.demo.data.entities.Account
+import com.rubenlr.demo.data.entities.Asset
 import com.rubenlr.demo.data.entities.Transaction
+import com.rubenlr.demo.data.entities.User
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 
 @RepositoryTest
 class TransactionRepositoryTest {
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Autowired
+    private lateinit var assetRepository: AssetRepository
+
+    @Autowired
+    private lateinit var accountRepository: AccountRepository
 
     @Autowired
     private lateinit var transactionRepository: TransactionRepository
 
+    private lateinit var users: List<User>
+    private lateinit var assets: List<Asset>
+    private lateinit var accounts: List<Account>
     private lateinit var transactions: List<Transaction>
+
+    @BeforeEach
+    fun setUp() {
+        val gen = FakeDataProvider.getTransactions(2, 5)
+        users = userRepository.saveAllAndFlush(gen.users)
+        assets = assetRepository.saveAllAndFlush(gen.assets)
+        accounts = accountRepository.saveAllAndFlush(gen.accounts)
+        transactions = transactionRepository.saveAllAndFlush(gen.transactions)
+    }
+
+    @Test
+    fun `should save and find transactions`() {
+        val savedTransactions = transactionRepository.findAll()
+
+        assertEquals(transactions, savedTransactions)
+    }
 }
